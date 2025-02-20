bccb97c904a5209312323094a2c29a70dfd66159
01kazu <davidjoshua026@gmail.com>
2025-02-20 19:43:14 +0100

    chore: add channel_id and command var to /jotform route

--- webhook.py
+++ webhook.py
@@ -88,4 +88,8 @@ async def send_message(channel_id: str, telex_format: dict):
 @router.post("/jotform")
 async def jotform(request: Request):
     await body = request.json()
+    channel_id = body.get("settings")[0].get("default")
+    print(channel_id)
+    command = body.get("message")
+    print(command)
     return {"status": "success", "message": "Test Passed Successfully"}
