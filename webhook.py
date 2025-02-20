29745b2485a36a5b7335a751e8c337219ff9fbcb
01kazu <davidjoshua026@gmail.com>
2025-02-20 19:25:10 +0100

    chore: test /jotform functionality in telex

--- webhook.py
+++ webhook.py
@@ -5,56 +5,56 @@ from fastapi.responses import JSONResponse
 router = APIRouter()
 
 
-@router.post("/jotform")
-async def jotform(request: Request):
-    try:
-        # Remind the user to make sure channel ID is in the settings
-        body = await request.json()
+# @router.post("/jotform")
+# async def jotform(request: Request):
+#     try:
+#         # Remind the user to make sure channel ID is in the settings
+#         body = await request.json()
 
-        channel_id = body.get("settings")[0].get("default")
-        print(channel_id)
-        command = body.get("message")
-        if command is None:
-            telex_format = {
-                "event_name": "Invalid Command",
-                "message": f'Enter "/start" to activate JotForm Bot',
-                "status": "error",
-                "username": "JotForm Bot"
-            }
-            await send_message(channel_id, telex_format)
-            return JSONResponse(status_code = status.HTTP_400_BAD_REQUEST, 
-                                content = {"response": "Invalid command"})
+#         channel_id = body.get("settings")[0].get("default")
+#         print(channel_id)
+#         command = body.get("message")
+#         if command is None:
+#             telex_format = {
+#                 "event_name": "Invalid Command",
+#                 "message": f'Enter "/start" to activate JotForm Bot',
+#                 "status": "error",
+#                 "username": "JotForm Bot"
+#             }
+#             await send_message(channel_id, telex_format)
+#             return JSONResponse(status_code = status.HTTP_400_BAD_REQUEST, 
+#                                 content = {"response": "Invalid command"})
         
-        command = command.strip().replace("<p>", "").replace("</p>", "")
+#         command = command.strip().replace("<p>", "").replace("</p>", "")
 
-        if command == "/start":
-            jotform_url = f"https://telex-jotform.onrender.com/api/v1/jotform/{channel_id}" # Put API URL in settings
-            telex_format = {
-                "event_name": "Copy this url to your JotForm Webhook",
-                "message": f"{jotform_url}",
-                "status": "success",
-                "username": "JotForm Bot"
-            }
-            await send_message(channel_id, telex_format)
-            return JSONResponse(status_code = status.HTTP_200_OK, 
-                                content = {"response": "JotForm Bot has been activated"})
-        else:
-            telex_format = {
-                "event_name": "Invalid Command",
-                "message": f'Enter "/start" to activate JotForm Bot',
-                "status": "error",
-                "username": "JotForm Bot"
-            }
-            await send_message(channel_id, telex_format)
-            return JSONResponse(status_code = status.HTTP_400_BAD_REQUEST, 
-                                content = {"response": "Invalid command"})
-    except Exception as e:
-        print(f"Failed | error: {e}")
-        return {"status": "error", "message": str(e), "status": "error"}
+#         if command == "/start":
+#             jotform_url = f"https://telex-jotform.onrender.com/api/v1/jotform/{channel_id}" # Put API URL in settings
+#             telex_format = {
+#                 "event_name": "Copy this url to your JotForm Webhook",
+#                 "message": f"{jotform_url}",
+#                 "status": "success",
+#                 "username": "JotForm Bot"
+#             }
+#             await send_message(channel_id, telex_format)
+#             return JSONResponse(status_code = status.HTTP_200_OK, 
+#                                 content = {"response": "JotForm Bot has been activated"})
+#         else:
+#             telex_format = {
+#                 "event_name": "Invalid Command",
+#                 "message": f'Enter "/start" to activate JotForm Bot',
+#                 "status": "error",
+#                 "username": "JotForm Bot"
+#             }
+#             await send_message(channel_id, telex_format)
+#             return JSONResponse(status_code = status.HTTP_400_BAD_REQUEST, 
+#                                 content = {"response": "Invalid command"})
+#     except Exception as e:
+#         print(f"Failed | error: {e}")
+#         return {"status": "error", "message": str(e), "status": "error"}
 
     
 @router.get("/jotform/{channel_id}")
-async def jotform_notifications(request: Request, channel_id: str):
+async def jotform(request: Request, channel_id: str):
     try:
         # if form_data is None return 
         body = await request.body()
@@ -84,3 +84,18 @@ async def send_message(channel_id: str, telex_format: dict):
     async with httpx.AsyncClient() as client:
         response = await client.post(channel_webhook_url, json=telex_format)
         response.raise_for_status()
+
+
+@router.post("/jotform")
+def jotform(request: Request):
+    body = request.json()
+    print(body)
+    # response = requests.post(
+    #                 url,
+    #                 json=payload,
+    #                 headers={
+    #                     "Accept": "application/json",
+    #                     "Content-Type": "application/json"
+    #                 }
+    #             )
+    return {"status": "success", "message": "Test Passed Successfully"}
