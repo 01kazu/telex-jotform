9770d7d7138049b770f9617becdac305851ac9a0
01kazu <davidjoshua026@gmail.com>
2025-02-21 08:13:20 +0100

    chore: add conditional statement to /jotform route

--- webhook.py
+++ webhook.py
@@ -1,4 +1,4 @@
-import httpx
+import requests
 from fastapi import Request, status, APIRouter
 from fastapi.responses import JSONResponse
 
@@ -51,44 +51,12 @@ router = APIRouter()
 #     except Exception as e:
 #         print(f"Failed | error: {e}")
 #         return {"status": "error", "message": str(e), "status": "error"}
-
-    
-@router.get("/jotform/{channel_id}")
-async def jotform(request: Request, channel_id: str):
-    try:
-        # if form_data is None return 
-        body = await request.body()
-
-        form_data = await request.form()
-        form_title = form_data.get("formTitle")
-
-        # Find a way to get the channel webhook URL form telex
-        # url = f"https://ping.telex.im/v1/webhooks/{0195181a-e640-7f85-a0c6-d32f46804380}"
-        telex_format = {
-            "event_name": "Form Sent",
-            "message": f"{form_title} has been filled",
-            "status": "success",
-            "username": "JotForm Bot"
-        }
-        await send_message(channel_id, telex_format)
-        return JSONResponse(status_code = status.HTTP_200_OK, 
-                        content = {"response": f"{form_title} form has been filled"})
-    except Exception as e:
-        print(f"Failed | error: {e}")
-        return {"status": "error", "message": str(e)}
-    
-
-async def send_message(channel_id: str, telex_format: dict):
-    channel_webhook_url = f"https://ping.telex.im/v1/webhooks/{channel_id}" # Put ping telex in settings
-    async with httpx.AsyncClient() as client:
-        response = await client.post(channel_webhook_url, json=telex_format)
-        response.raise_for_status()
-
-
 @router.post("/jotform")
 async def jotform(request: Request):
     try:
+        print("start")
         body = await request.json()
+        print("step 2")
         channel_id = body.get("settings")[0].get("default")
         command = body.get("message")
         if command is None:
@@ -99,17 +67,19 @@ async def jotform(request: Request):
                 "username": "JotForm Bot"   
             }
             return response
+            # send_message(channel_id, response)
         
         command = command.strip().replace("<p>", "").replace("</p>", "")
         if command == "/start":
             jotform_url = f"https://telex-jotform.onrender.com/api/v1/jotform/{channel_id}" # Put API URL in settings
-            telex_format = {
+            response = {
                 "event_name": "Copy this url to your JotForm Webhook",
                 "message": f"{jotform_url}",
                 "status": "success",
                 "username": "JotForm Bot"
             }
-            return telex_format
+            return response
+            # send_message(channel_id, response)
         else:
             response = {
                 "event_name": "Invalid Command",
@@ -118,5 +88,39 @@ async def jotform(request: Request):
                 "username": "JotForm Bot"   
             }
             return response
+            # send_message(channel_id, response)
     except Exception as e:
-        return { "status": "error", "message": f"Test Failed Successfully: {e}"} 
+        return { "status": "error", "message": f"Test Failed: {e}"}
+
+    
+@router.get("/jotform/{channel_id}")
+async def jotform(request: Request, channel_id: str):
+    try:
+        form_data = await request.form()
+        form_title = form_data.get("formTitle")
+        print(channel_id)
+
+        # Find a way to get the channel webhook URL form telex
+        # url = f"https://ping.telex.im/v1/webhooks/{0195181a-e640-7f85-a0c6-d32f46804380}"
+        telex_format = {
+            "event_name": "Form Sent",
+            "message": f"hello has been filled",
+            "status": "success",
+            "username": "JotForm Bot"
+        }
+        send_message(channel_id, telex_format)
+        # return JSONResponse(status_code = status.HTTP_200_OK, 
+        #                 content = {"response": f"{form_title} form has been filled"})
+    except Exception as e:
+        print(f"Failed | error: {e}")
+        return {"status": "error", "message": str(e)}
+    
+
+def send_message(channel_id: str, telex_format: dict):
+    channel_webhook_url = f"https://ping.telex.im/v1/webhooks/{channel_id}" # Put ping telex in settings
+    response = requests.post(channel_webhook_url, 
+                                json=telex_format,
+                                headers={"Content-Type": "application/json"})
+    print(response.json())
+
+ 
